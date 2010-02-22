204349715740ce2446d9f2c7c1d7b7c226a3844e
dbarge <dbarge>
2010-02-22 20:10:04 +0000

    MT2 (MT2 for leptonic WW), MT2J ( MT2 for tt -> leptonic WW + N Jets), and related functions

--- MT2/MT2.cc
+++ MT2/MT2.cc
@@ -22,17 +22,9 @@ double MT2(
 //--- Set the masses -----------------------//
 
 	//pa[0] = v1.M();
-	if( v1.M2() >= 0 ){
-		pa[0] = (double) v1.M();
-	} else {
-		pa[0] = (double) -1*sqrt( -1*v1.M2() );
-	}
 	//pb[0] = v2.M();
-	if( v2.M2() >= 0 ){
-		pb[0] = (double) v2.M();
-	} else {
-		pb[0] = (double) -1*sqrt( -1*v2.M2() );
-	}
+ 	pa[0] = v1.M2() >= 0 ? v1.M() : 0.0;	
+	pb[0] = v2.M2() >= 0 ? v2.M() : 0.0;
 
 	/* Note on floating point precision & M^2 < 0:
 
@@ -43,36 +35,24 @@ double MT2(
 		
 			This is what is recommended by the MT2Utility.cc authors
 
-		- Now ( 3X ) when attempting to set pa[0] and pb[0] using LorentzVector::M() I get the following error:
+		- Now ( 3X ) floating point roundoff can lead to m^2 = E^2 - p^2 < 0, we choose to set M to zero for these cases 
+		- Remember that many or all doubles were changed to floats at the NtupleMaker level from 2X to 3X to save space.
+		- This presents with the following error from calling LorentzVector::M() :
 
 			dlopen error: /home/users/dbarge/cvs/UserCode/JRibnik/CMS2/NtupleMacros/CORE/mt2/example_looper/./ScanChain_C.so: undefined symbol: 
 			_ZN4ROOT4Math9GenVector5ThrowEPKc
-			Load Error: Failed to load Dynamic link library /home/users/dbarge/cvs/UserCode/JRibnik/CMS2/NtupleMacros/CORE/mt2/example_looper/./ScanChain_C.so
-			.
-			.
-			.
+			Load Error: Failed to load Dynamic link library /home/users/dbarge/cvs/UserCode/JRibnik/CMS2/NtupleMacros/CORE/mt2/example_looper/./ScanChain_C.so ...
 			/home/users/dbarge/cvs/UserCode/JRibnik/CMS2/NtupleMacros/CORE/mt2/example_looper/./ScanChain_C_ACLiC_dict.o(.gnu.linkonce.t._ZNK4ROOT4Math9PxPyPzE4DIfE1MEv+0x4e): In function `ROOT::Math::PxPyPzE4D<float>::M() const':
 			: undefined reference to `ROOT::Math::GenVector::Throw(char const*)'
 
-			Remember that many or all doubles were changed to floats at the NtupleMaker level from 2X to 3X to save space.
-
-			Jmu said it best:
-
-			"As for your error, it's floating point roundoff.  You're asking for the mass.
-			So first you calculate E * E - p * p.  E and p are both big numbers, so you can
-			get a negative result if they're close enough together.  Then when you ask for
-			m(), the LorentzVector class sees that m^2 < 0 and throws an exception.
+			JMu said it best:
 			
-			The reason you see a linker error rather than an exception is a delicious morsel
+			"The reason you see a linker error rather than an exception is a delicious morsel
 			of ROOT rootiness.  The LorentzVector code is all included (because it's
 			templated in all those funny ways), so it doesn't require linking to any
 			library.  Except for the exceptions.  Those live in a library.  If you don't
-			link it in, you get a linker error.
+			link it in, you get a linker error."
 			
-			That probably doesn't interest you as much as how to solve the problem.
-			Basically, ask for m2(), see if it's negative, and if it is, use
-			-sqrt(-p4.m2())."
-
 			- For the leptonic (WW) mt2 distribution, we could just set the masses to 0 since we're in the massless limit anyway (excluding taus)
 			- The N jets (tt) mt2 distribution is significantly affected because the b quark mass is 4 GeV so the mass must be set properly
 	*/
@@ -107,6 +87,10 @@ double MT2J(
 	const ROOT::Math::LorentzVector<ROOT::Math::PxPyPzE4D<float> > p4_lepton_2,
 	const vector<ROOT::Math::LorentzVector<ROOT::Math::PxPyPzE4D<float> > > vect_p4_jets
 ){
+	if( vect_p4_jets.size() < 2 ){
+		cout << "MT2.cc: error MT2J called with less than 2 jets... returning mt2 value of -1.0";
+		return -1.0; 
+	}
 	double mt2_min = std::numeric_limits<double>::max();
 	for(unsigned int j1=0; j1 < vect_p4_jets.size(); j1++){
 	for(unsigned int j2=0; j2 < vect_p4_jets.size(); j2++){
