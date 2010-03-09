aeb0f26aff69f8d3f12ad4c25dd726aaa3adc169
dbarge <dbarge>
2010-03-09 05:10:21 +0000

    fixed mu_ttbar denominator def

--- fakerates.cc
+++ fakerates.cc
@@ -48,11 +48,13 @@ else if ( version == mu_ttbar ){
       return false;
     }
     if ( TMath::Abs(cms2.mus_p4()[index].eta()) > 2.5)  return false; // eta cut
-    if (cms2.mus_gfit_chi2().at(index)/cms2.mus_gfit_ndof().at(index) >= 10) return false; //glb fit chisq
+    //if (cms2.mus_gfit_chi2().at(index)/cms2.mus_gfit_ndof().at(index) >= 10) return false; //glb fit chisq
+    if (cms2.mus_gfit_chi2().at(index)/cms2.mus_gfit_ndof().at(index) >= 50) return false; //glb fit chisq
     if (((cms2.mus_type().at(index)) & (1<<1)) == 0)    return false; // global muon
     if (((cms2.mus_type().at(index)) & (1<<2)) == 0)    return false; // tracker muon
     if (cms2.mus_validHits().at(index) < 11)            return false; // # of tracker hits
-    if (TMath::Abs(cms2.mus_d0corr().at(index)) > 0.02) return false; // d0 from beamspot
+    //if (TMath::Abs(cms2.mus_d0corr().at(index)) > 0.02) return false; // d0 from beamspot
+    if (TMath::Abs(cms2.mus_d0corr().at(index)) > 0.2) return false; // d0 from beamspot
     return true;
 } 
 else {
