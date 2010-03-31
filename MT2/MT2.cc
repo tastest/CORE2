8ea06b75c557b0dcfef008248fab060baae9d3ec
dbarge <dbarge>
2010-03-31 17:18:05 +0000

    added argument to MT2(), MT2J() for invisible particle mass with default value 0.0

--- MT2/MT2.cc
+++ MT2/MT2.cc
@@ -5,66 +5,66 @@
 
 // MT2( MET_MAGNITUDE, MET_PHI, P4_LEPTON_1, P4_LEPTON_2, MASS_INVISIBLE_PARTICLE )
 double MT2(
-	const float met,
-	const float metPhi,
-	const ROOT::Math::LorentzVector<ROOT::Math::PxPyPzE4D<float> > v1,
-	const ROOT::Math::LorentzVector<ROOT::Math::PxPyPzE4D<float> > v2,
+  const float met,
+  const float metPhi,
+  const ROOT::Math::LorentzVector<ROOT::Math::PxPyPzE4D<float> > v1,
+  const ROOT::Math::LorentzVector<ROOT::Math::PxPyPzE4D<float> > v2,
   float invisible_particle_mass
 ){
 
-/* code here follows documentation in MT2Utility.cc */
+//--- code here follows documentation in MT2Utility.cc ---//
 
-	// initialize arrays for lepton 1, lepton 2, MET
-	double pa[3]    = {0,0,0};
-	double pb[3]    = {0,0,0};
-	double pmiss[3] = {0,0,0};
+  // initialize arrays for lepton 1, lepton 2, MET
+  double pa[3]    = {0,0,0};
+  double pb[3]    = {0,0,0};
+  double pmiss[3] = {0,0,0};
 
   // Set the masses
- 	pa[0] = v1.M2() >= 0 ? v1.M() : 0.0;	
-	pb[0] = v2.M2() >= 0 ? v2.M() : 0.0;
+  pa[0] = v1.M2() >= 0 ? v1.M() : 0.0;
+  pb[0] = v2.M2() >= 0 ? v2.M() : 0.0;
 
-	// set the transverse momenta for the leptons & MET
-	pa[1]     = (double) v1.Px();
-	pa[2]     = (double) v1.Py();
-	pb[1]     = (double) v2.Px();
-	pb[2]     = (double) v2.Py();
-	pmiss[0]  = 0.0;	                      // not used
-	pmiss[1]  = (double) met*cos(metPhi);
-	pmiss[2]  = (double) met*sin(metPhi);
+  // set the transverse momenta for the leptons & MET
+  pa[1]     = (double) v1.Px();
+  pa[2]     = (double) v1.Py();
+  pb[1]     = (double) v2.Px();
+  pb[2]     = (double) v2.Py();
+  pmiss[0]  = 0.0;                      // not used
+  pmiss[1]  = (double) met*cos(metPhi);
+  pmiss[2]  = (double) met*sin(metPhi);
 
-	// instantiate mt2 class, set momenta and mass of invisible particle
-	mt2_bisect::mt2 mt2_event;
-	mt2_event.set_momenta( pa, pb, pmiss );
-	mt2_event.set_mn( invisible_particle_mass );
+  // instantiate mt2 class, set momenta and mass of invisible particle
+  mt2_bisect::mt2 mt2_event;
+  mt2_event.set_momenta( pa, pb, pmiss );
+  mt2_event.set_mn( invisible_particle_mass );
 
   //
-	return mt2_event.get_mt2();
+  return mt2_event.get_mt2();
 }
 
 // MT2J( MET_MAGNITUDE, MET_PHI, P4_LEPTON_1, P4_LEPTON_2, VECT_P4_Jets, MASS_INVISIBLE_PARTICLE, MT2_CALCULATION_METHOD )
 double MT2J(
-	const float met,
-	const float metPhi,
-	const ROOT::Math::LorentzVector<ROOT::Math::PxPyPzE4D<float> > p4_lepton_1,
-	const ROOT::Math::LorentzVector<ROOT::Math::PxPyPzE4D<float> > p4_lepton_2,
-	const vector<ROOT::Math::LorentzVector<ROOT::Math::PxPyPzE4D<float> > > vect_p4_jets,
+  const float met,
+  const float metPhi,
+  const ROOT::Math::LorentzVector<ROOT::Math::PxPyPzE4D<float> > p4_lepton_1,
+  const ROOT::Math::LorentzVector<ROOT::Math::PxPyPzE4D<float> > p4_lepton_2,
+  const vector<ROOT::Math::LorentzVector<ROOT::Math::PxPyPzE4D<float> > > vect_p4_jets,
   float invisible_particle_mass,
   enum enum_mt2_method method_mt2
 ){
-	if( vect_p4_jets.size() < 2 ){
-		cout << "MT2.cc: error MT2J called with less than 2 jets... returning mt2 value of -1.0" << endl;
-		return -1.0; 
-	}
-	double mt2_min = std::numeric_limits<double>::max();
-	for(unsigned int j1=0; j1 < vect_p4_jets.size(); j1++){
-	for(unsigned int j2=0; j2 < vect_p4_jets.size(); j2++){
-		if(j1==j2) continue;
-		double mt2_j1_j2 = MT2( met, metPhi, p4_lepton_1 + vect_p4_jets.at(j1), p4_lepton_2 + vect_p4_jets.at(j2) );
-		if(mt2_j1_j2 < mt2_min){
-			mt2_min = mt2_j1_j2;
-		}
-	}}
-	return mt2_min;
+  if( vect_p4_jets.size() < 2 ){
+    cout << "MT2.cc: error MT2J called with less than 2 jets... returning mt2 value of -1.0" << endl;
+    return -1.0; 
+  }
+  double mt2_min = std::numeric_limits<double>::max();
+  for(unsigned int j1=0; j1 < vect_p4_jets.size(); j1++){
+  for(unsigned int j2=0; j2 < vect_p4_jets.size(); j2++){
+    if(j1==j2) continue;
+    double mt2_j1_j2 = MT2( met, metPhi, p4_lepton_1 + vect_p4_jets.at(j1), p4_lepton_2 + vect_p4_jets.at(j2) );
+    if(mt2_j1_j2 < mt2_min){
+      mt2_min = mt2_j1_j2;
+    }
+  }}
+  return mt2_min;
 }
 
 // Function to apply Pt, Eta, and lep-jet dR selections on jets
--- MT2/MT2.h
+++ MT2/MT2.h
@@ -1,21 +1,26 @@
 #include "MT2Utility.cc"
 #include "Math/LorentzVector.h"
 
-// leptonic (WW) mt2 declaration
+enum enum_mt2_method { BISECT, GRID };
+
+// MT2 declaration
 double MT2(
 	const float,
 	const float,
 	const ROOT::Math::LorentzVector<ROOT::Math::PxPyPzE4D<float> >,
-	const ROOT::Math::LorentzVector<ROOT::Math::PxPyPzE4D<float> >
+	const ROOT::Math::LorentzVector<ROOT::Math::PxPyPzE4D<float> >,
+  float = 0.0
 );
 
-// N jets (tt) mt2 declaration
+// MT2J declaration
 double MT2J(
 	const float,
 	const float,
 	const ROOT::Math::LorentzVector<ROOT::Math::PxPyPzE4D<float> >,
 	const ROOT::Math::LorentzVector<ROOT::Math::PxPyPzE4D<float> >,
-	const vector<ROOT::Math::LorentzVector<ROOT::Math::PxPyPzE4D<float> > >
+	const vector<ROOT::Math::LorentzVector<ROOT::Math::PxPyPzE4D<float> > >,
+  float = 0.0,
+  enum_mt2_method = BISECT
 );
 
 vector<ROOT::Math::LorentzVector<ROOT::Math::PxPyPzE4D<float> > > CleanJets(
