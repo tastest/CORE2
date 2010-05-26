63d3b64e4b41b48a65eb1e2ecf9006fa70572aff
slava77 <slava77>
2010-05-26 03:55:10 +0000

    making a mess in one place, just want this to work

---
+++ MT2.h
@@ -0,0 +1,25 @@
+#ifndef MT2_H
+#define MT2_H
+
+#include "MT2Utility.cc"
+#include "Math/LorentzVector.h"
+
+// leptonic (WW) mt2 declaration
+double MT2(
+	const float,
+	const float,
+	const ROOT::Math::LorentzVector<ROOT::Math::PxPyPzE4D<float> >,
+	const ROOT::Math::LorentzVector<ROOT::Math::PxPyPzE4D<float> >
+);
+
+// N jets (tt) mt2 declaration
+double MT2J(
+	const float,
+	const float,
+	const ROOT::Math::LorentzVector<ROOT::Math::PxPyPzE4D<float> >,
+	const ROOT::Math::LorentzVector<ROOT::Math::PxPyPzE4D<float> >,
+	const vector<ROOT::Math::LorentzVector<ROOT::Math::PxPyPzE4D<float> > >
+);
+
+
+#endif
