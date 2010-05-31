d8325a0e6fd355eca28aa8bc13131234a6fe3146
dbarge <dbarge>
2010-05-31 08:18:19 +0000

    fakerates.cc is depricated

--- fakerates.cc
+++ fakerates.cc
@@ -6,44 +6,44 @@
 
 // muons
 bool isFakeableMuon (int index, enum fakeRateVersion version){
-	cout << "fakerates.cc is depricated. Fakeable object definitions are now in muonSelections.cc" << endl;
+	cout << endl  << "fakerates.cc is depricated. Fakeable object definitions are now in muonSelections.cc" <<  endl << endl;
 	gSystem->Exit(1);
 	return false;
 } 
 double muFakeProb (int i_mu, enum fakeRateVersion version){
-	cout << "fakerates.cc is depricated. Fakeable object definitions are now in muonSelections.cc" << endl;
+	cout << endl  << "fakerates.cc is depricated. Fakeable object definitions are now in muonSelections.cc" <<  endl << endl;
 	gSystem->Exit(1);
 	return 0;
 }
 double muFakeProbErr (int i_mu, enum fakeRateVersion version){
-	cout << "fakerates.cc is depricated. Fakeable object definitions are now in muonSelections.cc" << endl;
+	cout << endl  << "fakerates.cc is depricated. Fakeable object definitions are now in muonSelections.cc" <<  endl << endl;
 	gSystem->Exit(1);
 	return 0;
 }
 void SetMuonFile(char * filename){
-	cout << "fakerates.cc is depricated. Fakeable object definitions are now in muonSelections.cc" << endl;
+	cout << endl  << "fakerates.cc is depricated. Fakeable object definitions are now in muonSelections.cc" <<  endl << endl;
 	gSystem->Exit(1);
 	return;
 }
 
 // electrons
 bool isFakeableElectron (int index, enum fakeRateVersion version){
-	cout << "fakerates.cc is depricated. Fakeable object definitions are now in electronSelections.cc" << endl;
+	cout << endl  << "fakerates.cc is depricated. Fakeable object definitions are now in electronSelections.cc" <<  endl << endl;
 	gSystem->Exit(1);
 	return false;
 }
 double elFakeProb (int i_el, enum fakeRateVersion version, char* sample ){
-	cout << "fakerates.cc is depricated. Fakeable object definitions are now in electronSelections.cc" << endl;
+	cout << endl  << "fakerates.cc is depricated. Fakeable object definitions are now in electronSelections.cc" <<  endl << endl;
 	gSystem->Exit(1);
 	return 0;
 }
 double elFakeProbErr (int i_el, enum fakeRateVersion version, char* sample ){
-	cout << "fakerates.cc is depricated. Fakeable object definitions are now in electronSelections.cc" << endl;
+	cout << endl  << "fakerates.cc is depricated. Fakeable object definitions are now in electronSelections.cc" <<  endl << endl;
 	gSystem->Exit(1);
 	return 0;
 }
 void SetElectronFile(char * filename){
-	cout << "fakerates.cc is depricated. Fakeable object definitions are now in electronSelections.cc" << endl;
+	cout << endl  << "fakerates.cc is depricated. Fakeable object definitions are now in electronSelections.cc" <<  endl << endl;
 	gSystem->Exit(1);
 	return;
 }
