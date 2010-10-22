15028dafa95bf052bf99666c84a460b38482edbf
benhoob <benhoob>
2010-10-22 20:38:22 +0000

    Fixed bug = true

--- photonSelections.cc
+++ photonSelections.cc
@@ -79,9 +79,9 @@ bool isSpikePhoton( const unsigned int index ) {
 
 int isGoodEMObject( const unsigned int index ){
 
-  if ( fabs( photons_p4().at(index).eta() > 1  )  )     return -1; //eta < 1
-  if ( fabs( photons_p4().at(index).pt()  < 22 )  )     return -1; //pt > 22 GeV
-  if ( photons_hOverE().at(index)         > 0.1 )       return -1; //h/e < 0.1
+  if ( fabs( photons_p4().at(index).eta() ) > 1   )     return -1; //eta < 1
+  if ( photons_p4().at(index).pt() < 22           )     return -1; //pt > 22 GeV
+  if ( photons_hOverE().at(index) > 0.1           )     return -1; //h/e < 0.1
  
   //spike cleaning
   float swiss   = photons_swissSeed().at(index);
