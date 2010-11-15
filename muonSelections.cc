9ee3cd60f8856d3a4c0399ccf0e036a1c14ccc60
kalavase <kalavase>
2010-11-15 22:12:53 +0000

    forgot glb hit cut in new TTbar selection

--- muonSelections.cc
+++ muonSelections.cc
@@ -109,7 +109,6 @@ bool muonIdNotIsolated(unsigned int index, SelectionType type){
 		  if (TMath::Abs(cms2.mus_d0corr().at(index)) > 0.02) return false; // d0 from beamspot
 		  return true;
 
-
 	 case NominalTTbarV2:
 		  if ( TMath::Abs(cms2.mus_p4()[index].eta()) > 2.5)  return false; // eta cut
 		  if (cms2.mus_gfit_chi2().at(index)/cms2.mus_gfit_ndof().at(index) >= 10) return false; //glb fit chisq
@@ -327,12 +326,14 @@ bool muonIdNotIsolated(unsigned int index, SelectionType type){
 		  return true;
 		  break;
 	 case NominalTTbar_pass6:
-		  if ( TMath::Abs(cms2.mus_p4()[index].eta()) > 2.4)  return false; // eta cut
+	          if ( TMath::Abs(cms2.mus_p4()[index].eta()) > 2.4)  return false; // eta cut
 		  if (cms2.mus_gfit_chi2().at(index)/cms2.mus_gfit_ndof().at(index) >= 10) return false; //glb fit chisq
 		  if (((cms2.mus_type().at(index)) & (1<<1)) == 0)    return false; // global muon
 		  if (((cms2.mus_type().at(index)) & (1<<2)) == 0)    return false; // tracker muon
 		  if (cms2.mus_validHits().at(index) < 11)            return false; // # of tracker hits
+		  if (cms2.mus_gfit_validSTAHits().at(index) == 0)    return false; // Glb fit must have hits in mu chambers
 		  if (TMath::Abs(cms2.mus_d0corr().at(index)) > 0.02) return false; // d0 from beamspot
+		  return true;
 		  return true;
 
 
