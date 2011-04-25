e345dc5e4bd0be563c64e42e0e1fb86f4272d6cd
fgolf <fgolf>
2011-04-25 19:21:44 +0000

    was including utilities.cc/jetSelections.cc : change to including utilities.h/jetSelections.h.  also, a number of variables were missing cms2. in front and the CL tool was complaining

--- photonSelections.cc
+++ photonSelections.cc
@@ -1,6 +1,6 @@
 #include "photonSelections.h"
-#include "jetSelections.cc"
-#include "utilities.cc"
+#include "jetSelections.h"
+#include "utilities.h"
 #include <iostream>
 #include "TSystem.h"
 #include "Math/VectorUtil.h"
@@ -11,43 +11,43 @@
 //-----------
 bool photonId( const unsigned int iPhoton, PhotonSelectionType type /* default defined in header */ ){
 
-  float dR_outer = 0.4;
-  float dR_inner = 0.05;
-  float sumHollow = 0;
+    float dR_outer = 0.4;
+    float dR_inner = 0.05;
+    float sumHollow = 0;
 
-  switch (type) {
+    switch (type) {
 
     case Yuri:
 
-      if( fabs( cms2.photons_p4().at(iPhoton).pt() ) < 10 ) return false;                                               // Pt
-      if( fabs( cms2.photons_p4().at(iPhoton).eta() ) > 1.479 ) return false;                                           // Eta
-      if( cms2.photons_ecalIso03().at(iPhoton) >= ( 4.2 + .004*cms2.photons_p4().at(iPhoton).pt() ) ) return false;       // ECAL Isolation
-      if( cms2.photons_hcalIso03().at(iPhoton) >= ( 2.2 + .001*cms2.photons_p4().at(iPhoton).pt() ) ) return false;       // HCAL Isolation
-      if( cms2.photons_hOverE().at(iPhoton)  >= 0.05 ) return false;                                                    // H over E
-      if( cms2.photons_sigmaIEtaIEta().at(iPhoton) >= 0.013 ) return false;                                             // Eta width
-      //if( cms2.photons_tkIsoHollow().at(iPhoton)  >= ( 2 + 0.001*cms2.photons_p4().at(iPhoton).pt() ) ) return false;   // Hollow
+        if( fabs( cms2.photons_p4().at(iPhoton).pt() ) < 10 ) return false;                                               // Pt
+        if( fabs( cms2.photons_p4().at(iPhoton).eta() ) > 1.479 ) return false;                                           // Eta
+        if( cms2.photons_ecalIso03().at(iPhoton) >= ( 4.2 + .004*cms2.photons_p4().at(iPhoton).pt() ) ) return false;       // ECAL Isolation
+        if( cms2.photons_hcalIso03().at(iPhoton) >= ( 2.2 + .001*cms2.photons_p4().at(iPhoton).pt() ) ) return false;       // HCAL Isolation
+        if( cms2.photons_hOverE().at(iPhoton)  >= 0.05 ) return false;                                                    // H over E
+        if( cms2.photons_sigmaIEtaIEta().at(iPhoton) >= 0.013 ) return false;                                             // Eta width
+        //if( cms2.photons_tkIsoHollow().at(iPhoton)  >= ( 2 + 0.001*cms2.photons_p4().at(iPhoton).pt() ) ) return false;   // Hollow
 
-      // Hollow Cone Isolation
-      for( unsigned int iTrack=0; iTrack < cms2.trks_trk_p4().size(); iTrack++ ){
-        float dR = ROOT::Math::VectorUtil::DeltaR( cms2.photons_p4().at(iPhoton), cms2.trks_trk_p4().at(iTrack) );
-        if( dR < dR_outer && dR > dR_inner ){
-          sumHollow += cms2.trks_trk_p4().at(iTrack).pt();         
+        // Hollow Cone Isolation
+        for( unsigned int iTrack=0; iTrack < cms2.trks_trk_p4().size(); iTrack++ ){
+            float dR = ROOT::Math::VectorUtil::DeltaR( cms2.photons_p4().at(iPhoton), cms2.trks_trk_p4().at(iTrack) );
+            if( dR < dR_outer && dR > dR_inner ){
+                sumHollow += cms2.trks_trk_p4().at(iTrack).pt();         
+            }
         }
-      }
-      if( sumHollow  >= ( 2.0 + 0.001*cms2.photons_p4().at(iPhoton).pt() ) ) return false;                              // Hollow
+        if( sumHollow  >= ( 2.0 + 0.001*cms2.photons_p4().at(iPhoton).pt() ) ) return false;                              // Hollow
 
-      if( isSpikePhoton(iPhoton) ) return false;                                                                        // Spike Removal
+        if( isSpikePhoton(iPhoton) ) return false;                                                                        // Spike Removal
 
-      return true;
+        return true;
 
-      break;
+        break;
 
     default:
-      std::cout << "photonID ERROR: requested photon type is not defined. Abort." << std::endl;
-      gSystem->Exit(1);
-      return false;
+        std::cout << "photonID ERROR: requested photon type is not defined. Abort." << std::endl;
+        gSystem->Exit(1);
+        return false;
 
-  }
+    }
 }
 
 //----------------------------
@@ -81,48 +81,48 @@ bool isSpikePhoton( const unsigned int index ) {
 
 int isGoodEMObject( const unsigned int index ){
 
-  //Minimum cut for making into babies. Tighter cut (0.95 was previous operating point) will be applied at template making.
-  const float neutralemfcut = 0.7;
+    //Minimum cut for making into babies. Tighter cut (0.95 was previous operating point) will be applied at template making.
+    const float neutralemfcut = 0.7;
 
-  //apply this cut at template creation time
-  //if ( fabs( photons_p4().at(index).eta() ) > 1   )     return -1; //eta < 1
-  if ( photons_p4().at(index).pt() < 22           )     return -1; //pt > 22 GeV
-  if ( photons_hOverE().at(index) > 0.1           )     return -1; //h/e < 0.1
+    //apply this cut at template creation time
+    //if ( fabs( photons_p4().at(index).eta() ) > 1   )     return -1; //eta < 1
+    if ( cms2.photons_p4().at(index).pt() < 22           )     return -1; //pt > 22 GeV
+    if ( cms2.photons_hOverE().at(index) > 0.1           )     return -1; //h/e < 0.1
 
-  //spike cleaning
-  if( isSpikePhoton( index ) )                          return -1;
+    //spike cleaning
+    if( isSpikePhoton( index ) )                          return -1;
 
-  //if photon survives to this point, find pfjet nearest photon
-  //require pt > 10 GeV pfjet, eta < 2.5 within dr < 0.3 of photon
+    //if photon survives to this point, find pfjet nearest photon
+    //require pt > 10 GeV pfjet, eta < 2.5 within dr < 0.3 of photon
 
-  float drmin       = 100;
-  int   iMatchedJet = -1;
+    float drmin       = 100;
+    int   iMatchedJet = -1;
 
-  for (unsigned int ijet = 0 ; ijet < pfjets_p4().size() ; ijet++) {
+    for (unsigned int ijet = 0 ; ijet < cms2.pfjets_p4().size() ; ijet++) {
           
-    LorentzVector vjet = pfjets_cor().at(ijet) * pfjets_p4().at(ijet);
-    LorentzVector vg   = photons_p4().at(index);
+        LorentzVector vjet = cms2.pfjets_cor().at(ijet) * cms2.pfjets_p4().at(ijet);
+        LorentzVector vg   = cms2.photons_p4().at(index);
           
-    if( vjet.pt()  < 10  )       continue;
-    if( fabs(vjet.eta()) > 2.5 )       continue;
+        if( vjet.pt()  < 10  )       continue;
+        if( fabs(vjet.eta()) > 2.5 )       continue;
     
-    float dr = dRbetweenVectors(vjet, vg);
+        float dr = dRbetweenVectors(vjet, vg);
     
-    if( dr < drmin ){
-      drmin       = dr;
-      iMatchedJet = ijet;
+        if( dr < drmin ){
+            drmin       = dr;
+            iMatchedJet = ijet;
+        }
     }
-  }
 
-  if( iMatchedJet < 0 ) return -2; //change -1 to -2, etc, so i can keep track in my looper of the cut which fails
-  if( drmin > 0.3 )     return -3;
+    if( iMatchedJet < 0 ) return -2; //change -1 to -2, etc, so i can keep track in my looper of the cut which fails
+    if( drmin > 0.3 )     return -3;
 
-  //require pfjet neutral EM fraction > 0.95
-  float emfrac = pfjets_neutralEmE().at(iMatchedJet) / pfjets_p4().at(iMatchedJet).energy();
-  if( emfrac < neutralemfcut )               return -4; 
-  //too tight
-  //if( !passesPFJetID(iMatchedJet) )                 return -5;
+    //require pfjet neutral EM fraction > 0.95
+    float emfrac = cms2.pfjets_neutralEmE().at(iMatchedJet) / cms2.pfjets_p4().at(iMatchedJet).energy();
+    if( emfrac < neutralemfcut )               return -4; 
+    //too tight
+    //if( !passesPFJetID(iMatchedJet) )                 return -5;
 
-  return iMatchedJet;
+    return iMatchedJet;
 
 }
