f62a9f3e31c411730f62e06191e422297f49d108
dbarge <dbarge>
2011-05-04 19:12:01 +0000

    fixed warnings

--- MT2/MT2.cc
+++ MT2/MT2.cc
@@ -1,3 +1,14 @@
+//---------------------------------------------------------------------------------
+// Filename      MT2.cc
+// Author:       dbarge
+// Created:         
+// Modified:     February 09 2011
+// Description:  Library to calculate "S-variables"
+//               MT2, MT2J, MT2||, MT2_|_, MCT, MCT||, MCT_|_, SQRT(s)_MIN
+// References:
+//
+//---------------------------------------------------------------------------------
+
 #include "MT2.h"
 #include "math.h"
 #include "Math/VectorUtil.h"
@@ -51,6 +62,104 @@ double MT2(
   return mt2_event.get_mt2();
 }
 
+// MT2( MET_MAGNITUDE, MET_PHI, P4_LEPTON_1, P4_LEPTON_2, GRID_HALF_WIDTH, GRID_SPACING,MASS_INVISIBLE_PARTICLE )
+TMt2* MT2_GRID(
+  const float met,
+  const float metPhi,
+  const ROOT::Math::LorentzVector<ROOT::Math::PxPyPzE4D<float> > v1,
+  const ROOT::Math::LorentzVector<ROOT::Math::PxPyPzE4D<float> > v2,
+  int grid_size,
+  int grid_spacing,
+  float invisible_particle_mass,
+  bool verbose
+){
+
+  if( invisible_particle_mass != 0 ){
+    cout << "ERROR: Non zero invisible particle masses are not implemented." << endl;
+    exit(1);
+  }
+
+  // Check input
+  if( grid_spacing > grid_size ){ 
+    cout << endl;
+    cout << "ERROR: Grid spacing must be larger than grid size... exiting." << endl;
+    cout << endl;
+    exit(1);
+  }
+
+  // Particle masses... M^2 < 0 sometimes due to floating poing imprecision
+  // leptons are in the massless limit anyway, set M = 0 when M^2 < 0
+  double mass1;
+  double mass2;
+  if( v1.M2() >= 0 ){
+    mass1 = v1.M();
+  } 
+  else {
+    mass1 = 0.0;
+    if(verbose) cout << "v1.M() < 0... Setting v1.M() = 0" << endl;
+  }
+  if( v2.M2() >= 0 ){
+    mass2 = v2.M();
+  } 
+  else{
+    mass2 = 0.0;
+    if(verbose) cout << "v2.M() < 0... Setting v1.M() = 0" << endl;
+  }
+
+  // Set transverse quantities 
+  double Et1    = v1.Et();
+  double Et2    = v2.Et();
+  double px1    = v1.Px();
+  double py1    = v1.Py();
+  double px2    = v2.Px();
+  double py2    = v2.Py();
+  double metx   = met*cos(metPhi);
+  double mety   = met*sin(metPhi);
+
+  // Initialize grid variables
+  double EtNu1 = 0;
+  double EtNu2 = 0;
+  double mtsq1 = 0;
+  double mtsq2 = 0;
+  double nu1Px = 0;
+  double nu1Py = 0;
+  double nu2Px = 0;
+  double nu2Py = 0;
+  double Max   = 0;
+  double Min   = std::numeric_limits<double>::max();
+
+  // Grid Minimization
+  for(int x = ( -1*grid_size ); x <= grid_size; x += grid_spacing ){
+  for(int y = ( -1*grid_size ); y <= grid_size; y += grid_spacing ){
+      double trial1_Px = x;
+      double trial1_Py = y;
+      double trial2_Px = ( metx - x );
+      double trial2_Py = ( mety - y );
+      EtNu1            = sqrt( pow( trial1_Px, 2 ) + pow( trial1_Py, 2 ) );
+      EtNu2            = sqrt( pow( trial2_Px, 2 ) + pow( trial2_Py, 2 ) );
+      mtsq1            = pow( mass1, 2 ) + 2 * ( Et1 * EtNu1 - px1 * trial1_Px - py1 * trial1_Py );
+      mtsq2            = pow( mass2, 2 ) + 2 * ( Et2 * EtNu2 - px2 * trial2_Px - py2 * trial2_Py );
+      Max              = max( sqrt( mtsq1 ), sqrt( mtsq2 ) );
+      if ( Max < Min ){ 
+        Min = Max;
+        nu1Px = trial1_Px;
+        nu1Py = trial1_Py;
+        nu2Px = trial2_Px;
+        nu2Py = trial2_Py;
+      }
+  } } 
+  if( Min == std::numeric_limits<double>::max() ){ 
+    cout << "MT2_GRID: Error could not find minimum." << endl; 
+    exit(1);
+  }
+  
+  //
+  TMt2* tmt2 = new TMt2( Min, nu1Px, nu1Py, nu2Px, nu2Py );
+  return tmt2;
+
+}
+
+
 // MT2J( MET_MAGNITUDE, MET_PHI, P4_LEPTON_1, P4_LEPTON_2, VECT_P4_Jets, MASS_INVISIBLE_PARTICLE, MT2_CALCULATION_METHOD )
 double MT2J(
   const float met,
@@ -83,6 +192,20 @@ double MT2J(
   }}
   return mt2_min;
 }
+
+/*
+double MT2J_GRID(
+  const float met,
+  const float metPhi,
+  const ROOT::Math::LorentzVector<ROOT::Math::PxPyPzE4D<float> > p4_lepton_1,
+  const ROOT::Math::LorentzVector<ROOT::Math::PxPyPzE4D<float> > p4_lepton_2,
+  const vector<ROOT::Math::LorentzVector<ROOT::Math::PxPyPzE4D<float> > > vect_p4_jets,
+  float,
+  bool
+){
+  return -1.0;
+}
+*/
 
 // Function to apply Pt, Eta, and lep-jet dR selections on jets
 Bool_t comparePt ( 
--- MT2/MT2.h
+++ MT2/MT2.h
@@ -7,12 +7,47 @@
 
 enum enum_mt2_method { BISECT, GRID };
 
+// MT2 Container
+class TMt2 {
+  private:
+    double Mt2_;
+    double Nu1Px_;
+    double Nu1Py_;
+    double Nu2Px_;
+    double Nu2Py_;
+  public:
+    TMt2( double, double, double, double, double );
+    inline double Mt2   (void) { return Mt2_;   }
+    inline double Nu1Px (void) { return Nu1Px_; }
+    inline double Nu1Py (void) { return Nu1Py_; }
+    inline double Nu2Px (void) { return Nu2Px_; }
+    inline double Nu2Py (void) { return Nu2Py_; }
+};
+TMt2::TMt2( double Mt2_arg, double Nu1Px_arg, double Nu1Py_arg, double Nu2Px_arg, double Nu2Py_arg ){
+  Mt2_   = Mt2_arg;
+  Nu1Px_ = Nu1Px_arg;
+  Nu1Py_ = Nu1Py_arg;
+  Nu2Px_ = Nu2Px_arg;
+  Nu2Py_ = Nu2Py_arg;
+}
+
 // MT2 declaration
 double MT2(
   const float,
   const float,
   const ROOT::Math::LorentzVector<ROOT::Math::PxPyPzE4D<float> >,
   const ROOT::Math::LorentzVector<ROOT::Math::PxPyPzE4D<float> >,
+  float = 0.0,
+  bool = false
+);
+
+TMt2* MT2_GRID(
+  const float,
+  const float,
+  const ROOT::Math::LorentzVector<ROOT::Math::PxPyPzE4D<float> >,
+  const ROOT::Math::LorentzVector<ROOT::Math::PxPyPzE4D<float> >,
+  int = 500,
+  int = 1,
   float = 0.0,
   bool = false
 );
@@ -28,6 +63,18 @@ double MT2J(
   enum_mt2_method = BISECT,
   bool = false
 );
+
+/*
+double MT2J_GRID(
+  const float,
+  const float,
+  const ROOT::Math::LorentzVector<ROOT::Math::PxPyPzE4D<float> >,
+  const ROOT::Math::LorentzVector<ROOT::Math::PxPyPzE4D<float> >,
+  const vector<ROOT::Math::LorentzVector<ROOT::Math::PxPyPzE4D<float> > >,
+  float = 0.0,
+  bool = false
+);
+*/
 
 vector<ROOT::Math::LorentzVector<ROOT::Math::PxPyPzE4D<float> > > CleanJets(
   const vector<ROOT::Math::LorentzVector<ROOT::Math::PxPyPzE4D<float> > >,
