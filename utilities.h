fe1d114f07e4c3c1c470f4de18ec0a04dc55437f
cerati <cerati>
2011-05-13 15:19:59 +0000

    add deltaPhi function

--- utilities.h
+++ utilities.h
@@ -16,6 +16,12 @@ double dRbetweenVectors(const LorentzVector & vec1,
 			const LorentzVector & vec2 );
 bool   sortByPt(const LorentzVector & vec1, const LorentzVector & vec2 );
 
+float deltaPhi( float phi1 , float phi2 ) {
+  float dphi = fabs( phi1 - phi2 );
+  if( dphi > TMath::Pi() ) dphi = TMath::TwoPi() - dphi;
+  return dphi;
+}
+
 class DorkyEvent;
 class DorkyEventIdentifier;
 
