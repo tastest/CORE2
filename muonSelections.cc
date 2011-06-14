e8de97ab1136fdcfca0e4b29af592271865a312b
cerati <cerati>
2011-06-14 12:55:38 +0000

    fix good global OR good tracker

--- muonSelections.cc
+++ muonSelections.cc
@@ -618,26 +618,33 @@ bool muonIdNotIsolated(unsigned int index, SelectionType type, int vertex_index)
         return true;
         break;
     case NominalSmurfV6:
+      {
         if (cms2.mus_p4().at(index).pt()<20){
 	  if (TMath::Abs(mud0PV_smurfV3(index)) >= 0.01)    return false; // d0 from pvtx
 	} else {
 	  if (TMath::Abs(mud0PV_smurfV3(index)) >= 0.02)    return false; // d0 from pvtx
 	}
         if ( TMath::Abs(cms2.mus_p4()[index].eta()) > 2.4)  return false; // eta cut
-	//recipe from boris
-        if (((cms2.mus_type().at(index)) & (1<<1)) != 0) { // global muon
-	  if (cms2.mus_nmatches().at(index)<2) return false;
-	  if (cms2.mus_gfit_chi2().at(index)/cms2.mus_gfit_ndof().at(index) >= 10) return false; //glb fit chisq
-	  if (cms2.mus_gfit_validSTAHits().at(index)==0 )     return false; // Glb fit must have hits in mu chambers
-	} else  if (((cms2.mus_type().at(index)) & (1<<2)) != 0) { // tracker muon
-	  if (cms2.mus_pid_TMLastStationTight().at(index) == 0 ) return false; // last station tight
-	} else return false;
         if (cms2.mus_validHits().at(index) < 11)            return false; // # of tracker hits  
         if (TMath::Abs(mudzPV_smurfV3(index)) >= 0.1)       return false; // dz from pvtx
         if (cms2.mus_ptErr().at(index)/cms2.mus_p4().at(index).pt()>0.1) return false;
         if (cms2.trks_valid_pixelhits().at(cms2.mus_trkidx().at(index))==0) return false;
-        return true;
+	//recipe from boris
+	bool goodMuonGlobalMuon = false;
+        if (((cms2.mus_type().at(index)) & (1<<1)) != 0) { // global muon
+	  goodMuonGlobalMuon = true;
+	  if (cms2.mus_nmatches().at(index)<2) goodMuonGlobalMuon = false;
+	  if (cms2.mus_gfit_chi2().at(index)/cms2.mus_gfit_ndof().at(index) >= 10) goodMuonGlobalMuon = false; //glb fit chisq
+	  if (cms2.mus_gfit_validSTAHits().at(index)==0 ) goodMuonGlobalMuon = false; // Glb fit must have hits in mu chambers
+	} 
+	bool goodMuonTrackerMuon = false;
+	if (((cms2.mus_type().at(index)) & (1<<2)) != 0) { // tracker muon
+	  goodMuonTrackerMuon = true;
+	  if (cms2.mus_pid_TMLastStationTight().at(index) == 0 ) goodMuonTrackerMuon = false; // last station tight
+	}
+	return goodMuonGlobalMuon || goodMuonTrackerMuon;
         break;
+      }
     default:
         std::cout << "muonID ERROR: requested muon type is not defined. Abort." << std::endl;
         return false;
