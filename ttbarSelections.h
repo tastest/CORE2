a8b78a9d8e1ae1e28183159a80f10415814feb33
dbarge <dbarge>
2011-12-10 05:36:21 +0000

    removed depricated functions

--- ttbarSelections.h
+++ ttbarSelections.h
@@ -6,30 +6,7 @@
 
 
 typedef ROOT::Math::LorentzVector<ROOT::Math::PxPyPzE4D<float> > LorentzVector;
-/******************************************************************************************/     
-// good lepton (either mu or electron, no isolation cuts)
-/******************************************************************************************/
-bool isGoodLeptonNoIso(int id, int lepIdx, bool applyAlignmentCorrection, bool removedEtaCutInEndcap=false);
 
-/******************************************************************************************/     
-// isolated lepton (either mu or electron)
-/******************************************************************************************/
-bool isGoodLeptonwIso(int id, int lepIdx, bool applyAlignmentCorrection, bool removedEtaCutInEndcap=false);
-
-/******************************************************************************************/     
-// are the leptons in the hypothesis good (all cuts but isolation?)
-/******************************************************************************************/
-bool isGoodHypNoIso(int hypIdx, bool applyAlignmentCorrection, bool removedEtaCutInEndcap=false);
-
-/******************************************************************************************/     
-// are the leptons in the hypothesis isolated?
-/******************************************************************************************/     
-bool isGoodHypwIso(int hypIdx, bool applyAlignmentCorrection, bool removedEtaCutInEndcap=false);
-
-/******************************************************************************************/     
-// is it a good jet?
-/******************************************************************************************/     
-bool isGoodDilHypJet(LorentzVector jetp4, unsigned int& hypIdx, double ptCut, double absEtaCut, double dRCut, bool muJetClean);
 
 /******************************************************************************************/     
 //return the MET and the MET phi, correcting for mus that are not corrected for by default
@@ -41,36 +18,6 @@ std::pair<float,float> getMet(const string algo, unsigned int hypIdx);
 /*****************************************************************************************/
 unsigned int selectHypByHighestSumPt(const vector<unsigned int> &v_goodHyps);
 
-/******************************************************************************************/     
-//corrects tcMET for mus that are not corrected for by default
-/******************************************************************************************/     
-//void correctTcMETForHypMus(unsigned int hypIdx, double& met, double& metPhi);
-
-/*****************************************************************************************/
-//passes the EGamma triggers
-/*****************************************************************************************/
-bool passEGTrigger(bool mc);
-/*****************************************************************************************/
-//passes the Muon triggers
-/*****************************************************************************************/
-bool passMuTrigger(bool mc);
-/*****************************************************************************************/
-//passes the Mu triggers
-/*****************************************************************************************/
-bool passMuTrigger(bool mc);
-
-/*****************************************************************************************/
-//get the impact parameter wrt the PV
-/*****************************************************************************************/
-double getd0wrtPV(LorentzVector p4, float d0);
-
-/*****************************************************************************************/
-//get the number of jets passing btag discriminator cuts
-// takes as arguments a vector of indices, the jet algorithm
-// and the btag discriminator
-// the working points are hard coded
-/*****************************************************************************************/
-int getNbtags(const vector<unsigned int> v_jetsIdx, const string jetAlgo, const string bTagDiscriminator);
 
 #endif
 
