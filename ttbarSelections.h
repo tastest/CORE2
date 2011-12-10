3b53035fa54cdf9381f591e9dbf35eda68d322e2
dbarge <dbarge>
2011-12-10 05:38:27 +0000

    removed depricated functions

--- ttbarSelections.h
+++ ttbarSelections.h
@@ -8,10 +8,10 @@
 typedef ROOT::Math::LorentzVector<ROOT::Math::PxPyPzE4D<float> > LorentzVector;
 
 
-/******************************************************************************************/     
-//return the MET and the MET phi, correcting for mus that are not corrected for by default
-/******************************************************************************************/     
-std::pair<float,float> getMet(const string algo, unsigned int hypIdx);
+///******************************************************************************************/     
+////return the MET and the MET phi, correcting for mus that are not corrected for by default
+///******************************************************************************************/     
+//std::pair<float,float> getMet(const string algo, unsigned int hypIdx);
 
 /*****************************************************************************************/
 //hypothesis disambiguation. Returns the hypothesis that has the highest sum Pt
