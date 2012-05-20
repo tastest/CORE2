651582a74428f42876ac7472a57be7443bca5743
fgolf <fgolf>
2012-05-20 02:29:43 +0000

    update some documentation

--- ssSelections.h
+++ ssSelections.h
@@ -122,27 +122,27 @@ namespace samesign {
 
 
 ///////////////////////////////////////////////////////////////////////////////////////////
-// get jets and perform overlap removal with numerator e/mu with pt > x (defaults are 10/5 GeV)
+// get jets and perform overlap removal with numerator e/mu with pt > x (defaults are 20/20 GeV)
 ///////////////////////////////////////////////////////////////////////////////////////////
     std::vector<LorentzVector> getBtaggedJets(int idx, enum JetType type, enum BtagType btag_type, float deltaR = 0.4, float min_pt = 40., float max_eta = 2.4, float mu_minpt = 20., float ele_minpt = 20., float rescale = 1.0);
 
 
 ///////////////////////////////////////////////////////////////////////////////////////////
 // get jets and apply an on-the-fly JEC and perform overlap removal with numerator
-// e/mu with pt > x (defaults are 10/5 GeV)
+// e/mu with pt > x (defaults are 20/20 GeV)
 ///////////////////////////////////////////////////////////////////////////////////////////
     std::vector<LorentzVector> getBtaggedJets(int idx, FactorizedJetCorrector* jet_corrector, enum JetType type, enum BtagType btag_type, float deltaR = 0.4, float min_pt = 40., float max_eta = 2.4, float mu_minpt = 20., float ele_minpt = 20., float rescale = 1.0);
 
 
 ///////////////////////////////////////////////////////////////////////////////////////////
-// get jets and perform overlap removal with numerator e/mu with pt > x (defaults are 10/5 GeV)
+// get b-tagged jets and perform overlap removal with numerator e/mu with pt > x (defaults are 20/20 GeV)
 ///////////////////////////////////////////////////////////////////////////////////////////
     std::vector<bool> getBtaggedJetFlags(int idx, enum JetType type, enum BtagType btag_type, float deltaR = 0.4, float min_pt = 40., float max_eta = 2.4, float mu_minpt = 20., float ele_minpt = 20., float rescale = 1.0);
 
 
 ///////////////////////////////////////////////////////////////////////////////////////////
-// get jets and apply an on-the-fly JEC and perform overlap removal with numerator
-// e/mu with pt > x (defaults are 10/5 GeV)
+// get b=tagged jets and apply an on-the-fly JEC and perform overlap removal with numerator
+// e/mu with pt > x (defaults are 20/20 GeV)
 ///////////////////////////////////////////////////////////////////////////////////////////
     std::vector<bool> getBtaggedJetFlags(int idx, FactorizedJetCorrector* jet_corrector, enum JetType type, enum BtagType btag_type, float deltaR = 0.4, float min_pt = 40., float max_eta = 2.4, float mu_minpt = 20., float ele_minpt = 20., float rescale = 1.0);
 
